f4eb6873e9d727c2ee3784de764538a1ad6b9f24
MAI <catalinaormenodiaz@gmail.com>
2023-09-21 09:56:07 -0300

    cmdo_proyecto terminado

--- cmdo_app1/views.py
+++ cmdo_app1/views.py
@@ -5,6 +5,13 @@ def vistaUno(request):
     html="""
     <h1 style="color:orange">Vista 1 App1</h1>
     <body><h1>Vista 2</h1><p>Este es el contenido de la vista uno.</p></body>
+        <style>
+        .parrafo-con-fondo {
+            background-color: #FFA500; 
+            padding: 10px;
+        }
+    </style>
+    <p class="parrafo-con-fondo">Este es un párrafo con un color de fondo naranja.</p>
     <img src="https://i.pinimg.com/564x/72/1f/71/721f71843f692f511a03f30a47d2764f.jpg" />
 
     """
--- cmdo_app2/views.py
+++ cmdo_app2/views.py
@@ -5,6 +5,13 @@ def vistaDos(request):
     html="""
     <h1 style="color:blue">Vista 1 App2</h1>
     <body><h1>Vista 2</h1><p>Este es el contenido de la vista dos.</p></body>
+    <style>
+        .parrafo-con-fondo {
+            background-color: #ffcc00; 
+            padding: 10px;
+        }
+    </style>
+    <p class="parrafo-con-fondo">Este es un párrafo con un color de fondo amarillo.</p>
     <img src="https://i.pinimg.com/564x/ba/01/72/ba017251aeb2ecac1f576fee21b40b93.jpg"/>
     """
     return HttpResponse(html)
